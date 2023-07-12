697973ff49f91ebb372be3b80cae4ee37aaae63a
Rajni bala <balatamna16@gmail.com>
2023-07-12 13:20:46 -0400

    inserted values for employees table

--- db/seed.sql
+++ db/seed.sql
@@ -17,7 +17,7 @@ INSERT INTO role (title, salary, department_id)
  VALUES ('Full Stack Web Developer', 100000, 1);
 
  INSERT INTO role (title, salary, department_id)
- VALUES ('Accounting Manager', 100000, 2);
+ VALUES ('Financial Controller', 100000, 2);
  INSERT INTO role (title, salary, department_id)
  VALUES ('Accountant', 80000, 2);
 
@@ -27,4 +27,19 @@ INSERT INTO role (title, salary, department_id)
  VALUES ('Sales Customer Service', 60000, 3);
 
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('William', 'Smith', 1);
+INSERT INTO employee (first_name, last_name, role_id,manager_id) 
+VALUES ('William', 'Smith', 1,3);
+INSERT INTO employee (first_name, last_name, role_id,manager_id) 
+VALUES ('Richard', 'Dennis', 2,1);
+INSERT INTO employee (first_name, last_name, role_id,manager_id) 
+VALUES ('Barbara', 'Deros', 3,NULL);
+INSERT INTO employee (first_name, last_name, role_id,manager_id) 
+VALUES ('Simran', 'Choudry', 4,3);
+INSERT INTO employee (first_name, last_name, role_id,manager_id) 
+VALUES ('Lovepreet', 'Kaur', 5,NULL);
+INSERT INTO employee (first_name, last_name, role_id,manager_id) 
+VALUES ('Sukhman', 'Kumar', 2,NULL);
+INSERT INTO employee (first_name, last_name, role_id,manager_id) 
+VALUES ('Shiv', 'Choudry', 4,7);
+INSERT INTO employee (first_name, last_name, role_id,manager_id) 
+VALUES ('Pawan', 'Cruise', 1,2);
