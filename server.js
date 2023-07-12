761d9e1fc12d4fd2c626ab68ab59b7a24db04a48
Rajni bala <balatamna16@gmail.com>
2023-07-12 17:14:17 -0400

    view employees function created

--- server.js
+++ server.js
@@ -1,85 +1,109 @@
-//Import packages
-const inquirer = require('inquirer');
-const mysql = require('mysql')
-const cTable = require('console.table');
+//dependencies required
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+require("console.table");
+//const sql = require("./sql");
 
-//Connect to database
-const db = mysql.createConnection(
-  {
+//mysql connection
+const connection = mysql.createConnection({
     host: 'localhost',
-    // MySQL username,
+
+   
+    port: 3306,
+
+    // Your username
     user: 'root',
 
+    // Your password
     password: 'Water12345@',
     database: 'employees_db'
-  },
-  console.log(`Connected to the employess_db.`)
-);
-
-db.connect(function (err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-
-  // runs the app
-  firstPrompt();
 });
 
-function firstPrompt() {
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+   
+    // runs the app
+    employeeTracker();
+});
+
+// function which prompts the user for what action they should take
+function employeeTracker() {
+
   inquirer
-      .prompt(
-          {
-              name: 'job',
-              type: 'list',
-              message: 'Which would you like to do?',
-              choices: [
-                "View All Department",
-                "View All Roles",
-                "View All Employees",
-                "Add a Department",
-                "Add a Role",
-                "Add an Employee",
-                "update an employee role"],
-          }
-      ).then(function ({ job }) {
-          switch (job) {
-              case 'View All Department':
-                  viewAlldepartment();
-                  break;
+    .prompt({
+      type: "list",
+      name: "task",
+      message: "Would you like to do?",
+      choices: [
+        "View All Departments",
+        "View All Roles",
+        "View All Employees",
+        "Add Department",
+        "Add Role",
+        "Add Employee",
+        "Update Employee Role",
+        "End"]
+    })
+    .then(function ({ task }) {
+      switch (task) {
+        case "View All Departments":
+          viewDepartment();
+          break;
 
-              case 'View all Roles':
-                viewAllRoles();
-                  break;
+          case "View All Employees":
+            viewEmployee();
+            break;
 
-                  case "View All Employees":
-                    viewAllEmployees();
-                    break;
-          
-                  case "Add a Department":
-                    addDepartment();
-                    break;
-          
-                  case "Add a Role":
-                    addRole();
-                    break;
-          
-                  case "Add an Employee":
-                    addanEmployee();
-                    break;
-          
-                    case "Update an Employe Role":
-                    updateanEmployeeRole();
-                    break;
+            case "View All Roles":
+              viewRoles();
+              break;
 
-                  case "Exit":
-                    db.end();
-                    break;
-          }
+          case "Add Department":
+          addDepartment();
+          break;
 
-      })
-};
+          case "Add Role":
+          addRole();
+          break;
 
+         case "Add Employee":
+          addEmployee();
+          break;
 
+          case "Update Employee Role":
+          updateEmployeeRole();
+          break;
 
+          case "End":
+          connection.end();
+          break;
+      }
+    });
+}
 
+//View Employees/ READ all, SELECT * FROM
+function viewEmployee() {
+  console.log("Viewing employees\n");
 
+  var query =
+    `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+  FROM employee e
+  LEFT JOIN role r
+	ON e.role_id = r.id
+  LEFT JOIN department d
+  ON d.id = r.department_id
+  LEFT JOIN employee m
+	ON m.id = e.manager_id`
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    console.table(res);
+    console.log("Employees viewed!\n");
+
+    employeeTracker();
+  });
+
+}
 
